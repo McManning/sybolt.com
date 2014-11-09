389434133b75df1163e290cb274ad43f9d3a1df2
Chase McManning <cmcmanning@gmail.com>
2014-11-08 21:36:40 -0500

    Swapping home and test views, so we can clear out test stuff from the home

--- js/views/home.js
+++ js/views/home.js
@@ -4,41 +4,16 @@ define([
     'underscore',
     'backbone',
     'app',
-    'collections/profiles',
-    'text!templates/home.html',
-], function($, _, Backbone, App, ProfilesCollection, homeTemplate) {
+    'text!templates/home.html'
+], function($, _, Backbone, App, homeTemplate) {
     'use strict';
     
     var HomeView = Backbone.View.extend({
         template: _.template(homeTemplate),
-
-        events: {
-            "click #testLink": "testLinkAction"
-        },
-        
-        testLinkAction: function() {
-            alert('test link!');
-        },
         
         initialize: function() {
             
-            this.collection = new ProfilesCollection();
-            //this.collection.add({ id: 1, name: 'Chase' });
-            //this.collection.add({ id: 2, name: 'Mark Meltzer' });
-            
-            // Force a redraw of the view whenever our collection changes
-            this.collection.bind("change add remove reset", this.render, this);
-            
-            this.collection.fetch({
-                success: function(collection, response) {
-                
-                    // Redraw our view with the updated collection
-                   //self.render();
-                },
-                error: function(collection, response) {
-                    alert('shit went wrong');
-                }
-            });
+            // Will have sub-views associated with it. Or something.
         },
         
         close: function() {
@@ -47,22 +22,16 @@ define([
             // And: http://metametadata.wordpress.com/2013/06/17/backbone-js-1-0-0-nested-view-memory-leak/
             console.log('Kill: ', this);
             
-            this.collection.reset(); // Clear collection of profiles
             this.remove(); // Destroy this
         },
         
         render: function() {
             
-            // Reconfigure our layout of the header and footer
-            App.headerView.setStyle('default');
-            
-            var data = {}; // do stuff
-            var compiled = this.template({
-                message: 'Hello world!',
-                profiles: this.collection.models
-            });
+            var data = {};
+            var compiled = this.template(data);
             
             this.$el.html(compiled);
+            
             return this;
         }
     });
--- js/views/test.js
+++ js/views/test.js
@@ -4,16 +4,41 @@ define([
     'underscore',
     'backbone',
     'app',
-    'text!templates/test.html'
-], function($, _, Backbone, App, testTemplate) {
+    'collections/profiles',
+    'text!templates/test.html',
+], function($, _, Backbone, App, ProfilesCollection, testTemplate) {
     'use strict';
     
     var TestView = Backbone.View.extend({
         template: _.template(testTemplate),
+
+        events: {
+            "click #testLink": "testLinkAction"
+        },
+        
+        testLinkAction: function() {
+            alert('test link!');
+        },
         
         initialize: function() {
             
-            // Will have sub-views associated with it. Or something.
+            this.collection = new ProfilesCollection();
+            //this.collection.add({ id: 1, name: 'Chase' });
+            //this.collection.add({ id: 2, name: 'Mark Meltzer' });
+            
+            // Force a redraw of the view whenever our collection changes
+            this.collection.bind("change add remove reset", this.render, this);
+            
+            this.collection.fetch({
+                success: function(collection, response) {
+                
+                    // Redraw our view with the updated collection
+                   //self.render();
+                },
+                error: function(collection, response) {
+                    alert('shit went wrong');
+                }
+            });
         },
         
         close: function() {
@@ -22,16 +47,22 @@ define([
             // And: http://metametadata.wordpress.com/2013/06/17/backbone-js-1-0-0-nested-view-memory-leak/
             console.log('Kill: ', this);
             
+            this.collection.reset(); // Clear collection of profiles
             this.remove(); // Destroy this
         },
         
         render: function() {
             
-            var data = {};
-            var compiled = this.template(data);
+            // Reconfigure our layout of the header and footer
+            App.headerView.setStyle('default');
+            
+            var data = {}; // do stuff
+            var compiled = this.template({
+                message: 'Hello world!',
+                profiles: this.collection.models
+            });
             
             this.$el.html(compiled);
-            
             return this;
         }
     });
