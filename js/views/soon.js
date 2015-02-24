da32b7fcde12b3ed6a26adc7601f346a55ee6fc4
Chase McManning <cmcmanning@gmail.com>
2015-02-24 17:42:08 -0500

    Adding description of upcoming features in the Coming Soon section

--- js/views/soon.js
+++ js/views/soon.js
@@ -8,9 +8,22 @@ define([
 ], function($, _, Backbone, App, Template) {
     'use strict';
     
+
+
     var PrivacyView = App.View.extend({
         template: _.template(Template),
         
+        events: {
+            'click #show-more-info': 'onClickShowMoreInfo'
+        },
+
+        onClickShowMoreInfo: function() {
+
+            this.$el.find('#more-info').show('slow');
+
+            return false;
+        },
+
         render: function() {
             
             // Reconfigure our layout of the header and footer
