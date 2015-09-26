81a4cefdaebe4109e92d2686cbdbe4c61f1aab82
Chase McManning <cmcmanning@gmail.com>
2015-09-26 13:04:35 -0400

    Adding a regex router and "improving" config loading

--- sybolt/__init__.py
+++ sybolt/__init__.py
@@ -1,10 +1,29 @@
 
+import os
 from flask import Flask, render_template
+from werkzeug.routing import BaseConverter
+
 #from flask.ext.sqlalchemy import SQLAlchemy
 #from sybolt.live.routes import live as live_blueprint
 
 app = Flask(__name__, static_url_path='')
-app.config.from_object('config.DevelopmentConfig')
+
+# Adding support for Regex in Flask
+# Via: http://stackoverflow.com/questions/5870188/does-flask-support-regular-expressions-in-its-url-routing
+# Example usage: @app.route('/<regex("[abcABC0-9]{4,6}"):uid>-<slug>/')
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
+# TODO: Temp way of specifying environment. Clean this up!
+if 'SYBOLT_ENV' not in os.environ:
+    raise Exception('You must specify SYBOLT_ENV')
+
+app.config.from_object('config.' + os.environ['SYBOLT_ENV'])
+
 #db = SQLAlchemy(app)
 
 # Register blueprint routes
