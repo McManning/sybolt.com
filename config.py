6199c8d3367787d5bf9a477d3605c91b49655d50
Chase McManning <cmcmanning@gmail.com>
2015-09-26 13:04:58 -0400

    Adjusting config domains

--- config.py
+++ config.py
@@ -2,15 +2,34 @@
 class Config(object):
     DEBUG = False
     PORT = 5000
-    MAX_CONTENT_LENGTH = 700 * 1024 # 700 KB
     SQLALCHEMY_DATABASE_URI = 'sqlite:///sybolt.db'
+
+    # Content that is normally served through Nginx
     ASSETS_PATH = '/path/to/static/content'
 
-class ProductionConfig(Config):
-    pass
-    
-class DevelopmentConfig(Config):
+class TuningForkProd(Config):
+    """ sybolt.com configuration (production)
+        Nginx will proxy access to the python process,
+        along with routing static file access to our dev
+        deployment path
+    """
+    PORT = 5000
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////var/local/sybolt/prod.db'
+    ASSETS_PATH = '/var/www/prod/sybolt'
+
+class TuningForkDev(Config):
+    """ dev.sybolt.com configuration.
+        Nginx will proxy access to the python process,
+        along with routing static file access to our dev
+        deployment path
+    """
     DEBUG = True
-    UPLOAD_FOLDER = 'uploads'
-    MAX_CONTENT_LENGTH = 900 * 1024 # 900 KB
+    PORT = 5001
+    ASSETS_PATH = '/var/www/dev/sybolt'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////var/local/sybolt/dev.db'
+
+class LocalDev(Config):
+    DEBUG = True
     ASSETS_PATH = 'X:\Workspace\Xampp\htdocs\sybolt-src'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///sybolt.db'
+    
